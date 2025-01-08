5598b630ae57530c9555651d7d80c2b8266a77ce
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2025-01-08 15:08:11 +0530

    ended the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import { useRef, useState, useCallback } from 'react';
+import { useRef, useState, useCallback, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
+import { fetchUserPlaces, updateUserPlaces } from './http.js';
+import Error from './components/Error.jsx';
 
 function App() {
   const selectedPlace = useRef();
 
+  const [isFetching,setIsFetching] = useState(false);
+
+  const [error ,setError] = useState();
+
   const [userPlaces, setUserPlaces] = useState([]);
+  const [errorUpdatingPlaces,setErrorUpdatingPlaces] = useState();
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(()=>{
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try{
+        const places = await fetchUserPlaces();
+        setUserPlaces(places);
+      }
+      catch(error){
+        setError({message : error.message || 'failed to fetch user places.' })
+      }
+      setIsFetching(false)
+    }
+
+    fetchPlaces();
+  },[])
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -22,7 +45,7 @@ function App() {
     setModalIsOpen(false);
   }
 
-  function handleSelectPlace(selectedPlace) {
+  async function handleSelectPlace(selectedPlace) {
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
@@ -32,18 +55,50 @@ function App() {
       }
       return [selectedPlace, ...prevPickedPlaces];
     });
+    try{
+      await updateUserPlaces([selectedPlace,...userPlaces]);
+    }
+    catch(error){
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({message: error.message || 'Failed to update places' })
+    }
+
+  }
+
+  function handleError(){
+    setErrorUpdatingPlaces(null);
   }
 
   const handleRemovePlace = useCallback(async function handleRemovePlace() {
     setUserPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
     );
+    try{
 
+      await updateUserPlaces(
+        userPlaces.filter(place=> place.id !== selectedPlace.current.id)
+      );
+    }
+    catch(error){
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({
+        message: error.message || 'Failed to delete place.'
+      })
+    }
+    
+     
     setModalIsOpen(false);
-  }, []);
+  }, [userPlaces]);
 
   return (
     <>
+      <Modal open={errorUpdatingPlaces} onClose={handleError}>
+        {errorUpdatingPlaces && (<Error
+          title="An error occured!"
+          message={errorUpdatingPlaces.message}
+          onConfirm={handleError}
+        />)}
+      </Modal>
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
@@ -60,9 +115,12 @@ function App() {
         </p>
       </header>
       <main>
+        {error && <Error title='An error occured!' message={error.message}  />}
         <Places
           title="I'd like to visit ..."
           fallbackText="Select the places you would like to visit below."
+          loadingText='Fetching your places...'
+          isLoading={isFetching}
           places={userPlaces}
           onSelectPlace={handleStartRemovePlace}
         />
--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,7 +4,7 @@ import Places from './Places.jsx';
 
 import Error from './Error.jsx';
 import {sortPlacesByDistance} from '../loc.js';
-
+import { fetchAvailablePlaces } from '../http.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching,setIsFetching] = useState(false);
@@ -16,16 +16,13 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
       try{
 
-        const response = await fetch('http://localhost:3000/places');
-        const resData = await response.json();
-  
-        if(!response.ok){
-          throw new Error('Failed to fetch places');
-        }
+        const places = await fetchAvailablePlaces();
 
 
         navigator.geolocation.getCurrentPosition((position)=>{
-          const sortedPlaces = sortPlacesByDistance(resData.places,position.coords.latitude,
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
             position.coords.longitude
           );
           setAvailablePlaces(sortedPlaces);
